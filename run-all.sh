e42f9afbcc12a3b63ac3ff847914bca27197bd39
maddenvvs <maddenvvs@gmail.com>
2021-12-06 14:02:12 +0400

    Make running all soluitons up to today

--- run-all.sh
+++ run-all.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-for day in $(seq -w 1 25)
+DECEMBER_FIRST=$(date -j '113000002021' +%s)
+TODAY=$(date +%s)
+DAYS_PAST=$((($TODAY - $DECEMBER_FIRST)/(3600*24)))
+if (($DAYS_PAST > 25)); then
+    $DAYS_PAST=25
+fi
+
+for day in $(seq -f "%02g" -w 1 $DAYS_PAST)
 do
     aoc2021 $day "./input/day-$day.input"
 done
